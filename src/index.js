5069897e965e669337fa41e6dbcfa2cca2c22a1e
Zabi-23 <zabi.sideqi@hotmail.com>
2024-06-14 23:22:19 +0200

    fixat index.js

--- src/index.js
+++ src/index.js
@@ -9,26 +9,17 @@ const authMiddleware = require('./middleware/authMiddleware');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-
 app.use(cors());
-
-
 app.use(express.json());
 
 app.use('/api/orders', orderRoutes); 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes); 
 
-
 app.use(errorHandler);
-
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
 
 
