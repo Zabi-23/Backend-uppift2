96b651c5e2950a3db2461878ca758fb5d85471d6
Zabi-23 <zabi.sideqi@hotmail.com>
2024-06-18 01:13:09 +0200

    prismaC

--- src/prismaC.js
+++ src/prismaC.js
@@ -1,7 +1,18 @@
-require('dotenv').config();
+/* require('dotenv').config();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 module.exports = prisma;
 
 
+ */
 
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+prisma.$connect()
+  .then(() => {
+    console.log("Database connected successfully.");
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
