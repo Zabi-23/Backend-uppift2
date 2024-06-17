e59b1f60ddc0c35b467e37231fdc272a17660045
Zabi-23 <zabi.sideqi@hotmail.com>
2024-06-18 01:54:22 +0200

    m

--- src/routes/product-routes.js
+++ src/routes/product-routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { getAllProducts, getOneProduct, addProduct, deleteProduct } = require('../controllers/product-controller');
-const authMiddleware = require('../middleware/authMiddleware');
+
 
 const router = express.Router();
 
-router.get('/products',  getAllProducts);  // This maps to /api/products
+router.get('/',  getAllProducts);  // This maps to /api/products
 router.get('/:id',  getOneProduct);         // This maps to /api/:id
 router.post('/',  addProduct);              // This maps to /api/
 router.delete('/:id',  deleteProduct);      // This maps to /api/:id
